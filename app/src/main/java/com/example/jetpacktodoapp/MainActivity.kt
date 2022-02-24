12d289c585a4f6fcf342409b0862e64fecc86afe
Dhananjay kapar <dhirajdhananjay4@gmail.com>
2022-02-24 08:09:42 +0530

    Base app

---
+++ app/src/main/java/com/example/jetpacktodoapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jetpacktodoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpacktodoapp.screens.mainscreen.MainScreen
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialTheme{
+        MainScreen()
+    }
+}
